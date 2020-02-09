1c47c9457990e16e9fe09453a0c1f706e201bed2
dmytrolehkyi-ua <dmytrolehkyi@gmail.com>
2020-02-09 16:46:14 +0200

    Added states to MainViewController. Implemented basic state transitions

--- AudioLab/ViewControllers/MainViewController.swift
+++ AudioLab/ViewControllers/MainViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    //MARK: - Public types
+
+    enum State {
+        case idle
+        case playing
+        case recording
+        case paused
+        case alarm
+    }
+
+    //MARK: - Public properties
+
+    var state: State = .idle {
+        didSet {
+            previousState = oldValue
+            didMove(to: state)
+        }
+    }
+
+    //MARK: - Private properties
+
+    private var previousState: State = .idle
+
     //MARK: - Outlets
 
     @IBOutlet private(set) var statusLabel: UILabel!
@@ -23,6 +46,16 @@ class MainViewController: UIViewController {
         setupUI()
     }
 
+    //MARK: - Actions
+
+    @IBAction func actionButtonPressed(_ sender: Any) {
+        if [.recording, .playing].contains(state) {
+            state = .paused
+        } else {
+            moveToNextState()
+        }
+    }
+
     //MARK: - Private methods
 
     private func setupUI() {
@@ -30,6 +63,51 @@ class MainViewController: UIViewController {
         actionButton.layer.cornerRadius = 5
         actionButton.setTitle("Play", for: .normal)
         statusLabel.text = "Idle"
+    }
+
+    private func moveToNextState() {
+        switch state {
+        case .idle: state = .playing
+        case .playing: state = .recording
+        case .paused: state = previousState
+        case .recording: state = .alarm
+        case .alarm: state = .idle
+        }
+    }
+
+    private func didMove(to state: State) {
+        switch state {
+        case .idle: didMoveToIdleState()
+        case .playing: didMoveToPlayingState()
+        case .paused: didMoveToPausedState()
+        case .recording: didMoveToRecordingState()
+        case .alarm: didMoveToAlarmState()
+        }
+    }
+
+    private func didMoveToIdleState() {
+        actionButton.setTitle("Play", for: .normal)
+        statusLabel.text = "Idle"
+    }
+
+    private func didMoveToPlayingState() {
+        actionButton.setTitle("Pause", for: .normal)
+        statusLabel.text = "Playing"
+    }
+
+    private func didMoveToRecordingState() {
+        actionButton.setTitle("Pause", for: .normal)
+        statusLabel.text = previousState == .playing ? "Playing" : "Recording"
+    }
+
+    private func didMoveToPausedState() {
+        actionButton.setTitle("Play", for: .normal)
+        statusLabel.text = "Paused"
+    }
+
+    private func didMoveToAlarmState() {
+        actionButton.setTitle("Play", for: .normal)
+        statusLabel.text = "Alarm"
     }
 }
 
